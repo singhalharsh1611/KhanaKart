7b85945a3b0f1f653e8910dcadc4a2863be6bb38
kickout <goyal08dhruv@gmail.com>
2025-01-22 15:33:04 +0530

    header and explore component added
    Signed-off-by: kickout <goyal08dhruv@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import Home from './components/Navbar/Home'
+
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/order" element={<PlaceOrder/>}/>
+      </Routes>
+      
     </div>
   )
 }
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import "./Home.css"
+import Header from '../../components/Header/Header.jsx'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu.jsx'
+import { set } from 'mongoose'
+const Home = () => {
+    const [category,setCategory]=useState("All");
+  return (
+    <div>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory} />
+    </div>
+  )
+}
+
+export default Home
