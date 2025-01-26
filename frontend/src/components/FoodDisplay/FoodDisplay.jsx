545ac4462971449da539919bceebf41873128d5c
kickout <goyal08dhruv@gmail.com>
2025-01-26 11:20:35 +0530

    Cart and PlaceOrder component added
    Signed-off-by: kickout <goyal08dhruv@gmail.com>

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,7 +10,7 @@ const FoodDisplay = ({ category}) => {
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          {console.log(category,index,item.categroy);}
+          // {console.log(category,index,item.categroy);}
           if (category==="All" || category===item.category) {
             return <FoodItem
             key={index}
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -19,10 +19,16 @@ const StoreContextProvider = (props) => {
     setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
   }
 
-useEffect(()=>{
-  console.log(cartItems);
-},[cartItems])
-
+  const getTotalCartAmount=()=>{
+    let totalAmount=0;
+    for(const item in cartItems){
+      if(cartItems[item]>0){
+        let itemInfo = food_list.find((product)=>product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  }
 
 
 
@@ -31,7 +37,8 @@ useEffect(()=>{
     cartItems,
     setCartItems,
     addtoCart,
-    removeFromCart
+    removeFromCart,
+    getTotalCartAmount
   };
   return (
     <StoreContext.Provider value={contextValue}>
