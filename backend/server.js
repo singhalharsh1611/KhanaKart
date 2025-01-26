f4a37c06b6c21adb2eb98347f43c7dceda129822
Harsh Singhal <singhalharsh1611@gmail.com>
2025-01-26 23:58:12 +0530

    setup mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
 import express from "express"
 import cors from "cors"
+import { connectDB } from "./config/db.js";
 
-// app congif
-const app=express();
-const port=4000;
+// app config
+const app = express();
+const port = 4000;
 
 // app middleware
 app.use(express.json())
 app.use(cors());
 
+// database connect
+connectDB();
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("API WORKING");
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server started on port :${port}`)
 })
