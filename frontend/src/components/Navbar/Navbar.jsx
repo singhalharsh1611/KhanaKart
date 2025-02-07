3bf48067a68ec3a65b607e5dc794185262609346
Harsh Singhal <singhalharsh1611@gmail.com>
2025-02-07 23:03:29 +0530

    updated css

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -38,9 +38,9 @@ const Navbar = ({setShowLogin}) => {
         <div className='navbar-profile'>
           <img src={assets.profile_icon} alt="" />
           <ul className='navbar-profile-dropdown'>
-            <li><img src={assets.bag_icon} alt="" /><p>orders</p></li>
+            <li><img src={assets.bag_icon} alt="" /><p>My Orders</p></li>
             <hr />
-            <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>logout</p></li>
+            <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
           </ul>
         </div>
         }
