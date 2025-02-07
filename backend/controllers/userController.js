398765f94db49b5f1c878ed9c1746a2e179b9141
Harsh Singhal <singhalharsh1611@gmail.com>
2025-02-07 23:50:59 +0530

    applied toast on login signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,22 +7,22 @@ import validator from "validator"
 
 export const loginUser = async(req,res)=>{
     const {email,password}=req.body;
-    console.log(req.body);
+    // console.log(req.body);
     try {
         const user =await userModel.findOne({email});
-        console.log(user);
+        // console.log(user);
         if(!user){
-            return res.json({success:false,message:"email doesnot exist"})
+            return res.json({success:false,message:"Email does not exist"})
         }
         const isMatch = await bcrypt.compare(password,user.password)
         if(!isMatch){
-            return res.json({success:false,message:"incorrect password"})
+            return res.json({success:false,message:"Incorrect Credentials"})
         }
         const token=createToken(user._id);
         res.json({success:true,token});
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:"error"});
+        res.json({success:false,message:"Error"});
     }
 }
 
@@ -33,19 +33,19 @@ const createToken=(id)=>{
 // register user
 
 export const registerUser = async(req,res)=>{
-    console.log(req.body);
+    // console.log(req.body);
     try {
         const {name,email,password}=req.body;
         
         const exist=await userModel.findOne({email});
         if(exist){
-            return res.json({success:false,message:"user already exists"})
+            return res.json({success:false,message:"User Already Exists"})
         }
         if(!validator.isEmail(email)){
-            return res.json({success:false,message:"Plase enter a valid email"})
+            return res.json({success:false,message:"Please enter a valid email"})
         }
         if(password.length<8){
-            return res.json({success:false,message:"please enter a strong password"});
+            return res.json({success:false,message:"Please enter a strong password"});
         }
         // hashing password
         const salt=await bcrypt.genSalt(10);
@@ -62,7 +62,7 @@ export const registerUser = async(req,res)=>{
 
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:"error"});
+        res.json({success:false,message:"Error"});
     }
     
 }
