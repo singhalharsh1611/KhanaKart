9c69a4478f9e5db8dd78b73d66f3ad4f0030d5fb
kickout <goyal08dhruv@gmail.com>
2025-02-07 18:09:30 +0530

    login signup done successfully
    Signed-off-by: kickout <goyal08dhruv@gmail.com>

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,19 @@
 import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = ({setShowLogin}) => {
 
     const [menu,setMenu] = useState("home");
     const {getTotalCartAmount,token,setToken } =useContext(StoreContext);
-    
+    const navigate=useNavigate();
+    const logout = ()=>{
+      localStorage.removeItem("token");
+      setToken("");
+      navigate("/");
+    }
   return (
     <div className='navbar'>
       <Link to="/"><img src={assets.logo} alt="" className="logo" /></Link>
@@ -26,11 +31,18 @@ const Navbar = ({setShowLogin}) => {
            
             <div className={getTotalCartAmount()?"dot":""}></div>
         </div>
-        {!token?
+        {!(token)?
         <button onClick={()=>setShowLogin(true)}>Sign In</button>
         :
         // <button onClick={setToken("")}>Sign Out</button>
-        <>profile</>
+        <div className='navbar-profile'>
+          <img src={assets.profile_icon} alt="" />
+          <ul className='navbar-profile-dropdown'>
+            <li><img src={assets.bag_icon} alt="" /><p>orders</p></li>
+            <hr />
+            <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>logout</p></li>
+          </ul>
+        </div>
         }
       </div>
     </div>
