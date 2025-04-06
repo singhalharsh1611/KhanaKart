d113288a5d656d7d4c97cf8628a75754752000d5
Harsh Singhal <70061407+singhalharsh1611@users.noreply.github.com>
2025-04-06 11:39:08 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,11 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url =
+  window.location.hostname === "localhost"
+    ? "http://localhost:4000"
+    : "https://khanakart-backend.onrender.com";
+
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
   const [userName, setUserName] = useState("");
