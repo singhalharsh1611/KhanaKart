7346d9b22070676c4e2cfb9e8bc3b4ec628de73e
Harsh Singhal <singhalharsh1611@gmail.com>
2025-04-06 12:08:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
-import { StoreContext } from "./context/StoreContext";
+import { StoreContext } from "./context/storeContext";
 import TokenIssue from "./pages/TokenIssue";
 import Verify from "./pages/Verify/Verify";
 import MyOrders from "./pages/MyOrders/MyOrders";
