248ff90c0994ce63cb3e5aedd918837e3f7ca84d
Harsh Singhal <singhalharsh1611@gmail.com>
2025-04-06 12:03:29 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -2,8 +2,6 @@ import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
-
-
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
