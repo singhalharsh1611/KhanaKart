4c703d36202876a928ff5b11230ff8200c1eb930
Harsh Singhal <singhalharsh1611@gmail.com>
2025-04-06 13:12:55 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order from frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = process.env.FRONTEND_URL;
 
   try {
     const newOrder = new orderModel({
