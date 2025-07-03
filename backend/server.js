b43e5a75a1a15100f3768de419517757437bb626
Harsh Singhal <singhalharsh1611@gmail.com>
2025-07-03 23:54:43 +0530

    fixed backend vercel

--- backend/server.js
+++ backend/server.js
@@ -12,43 +12,45 @@ import orderRouter from "./routes/orderRoute.js";
 
 
 
-    // app config
-    const app = express();
-    const port = process.env.PORT || 4000;
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
 
-    // app middleware
-    app.use(express.json())
-    app.use(cors({
-        origin: ['http://localhost:5173', 'http://localhost:5174', "https://khanakart.onrender.com", "https://khanakart-admin.onrender.com", process.env.BACKEND_URL, process.env.FRONTEND_URL, process.env.ADMIN_URL],
-        credentials: true
-    }));
-    app.use(session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true
-    }));
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use('/api/auth', userRouter);
+// app middleware
+app.use(express.json())
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174', process.env.FRONTEND_URL, process.env.ADMIN_URL, process.env.BACKEND_URL],
+    credentials: true
+}));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/api/auth', userRouter);
 
 
-    //initialize passport
-    passportSetup();
+//initialize passport
+passportSetup();
 
-    // database connect
-    connectDB();
+// database connect
+connectDB();
 
 //api endpoint
 app.use("/api/user", userRouter)
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"))
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
-    app.get("/", (req, res) => {
-        res.send("API WORKING");
-    })
+app.get("/", (req, res) => {
+    res.send("API WORKING");
+})
 
-    app.listen(port, () => {
-        console.log(`server started on port :${port}`)
-    })
+app.listen(port, () => {
+    console.log(`server started on port :${port}`)
+})
+
+export default app;
